ed813677d77fd0a5ec8156e4a71fd405a2e575ad
Kirill Voloshin <voloshin.k@husky.neu.edu>
2018-10-13 22:17:10 -0400

    2 user charts

--- website/js/music.js
+++ website/js/music.js
@@ -1,7 +1,11 @@
 var genreBarChart;
+var activeUserBarChart;
+var upvotedUserBarChart;
 
 $(function () {
     genreBar();
+    activeUsersBar();
+    upvotedUsersBar();
 })
 
 function genreBar() {
@@ -49,6 +53,103 @@ function genreBar() {
         },
         series: [{
             name: 'Submissions',
+            data: [50, 45, 24, 10, 2]
+        }]
+    });
+}
+
+function activeUsersBar() {
+    activeUserBarChart = Highcharts.chart('active-users-bar', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Most Active Users'
+        },
+        subtitle: {
+            text: 'By submission'
+        },
+        xAxis: {
+            categories: ['User1', 'User2', 'User3', 'User4', 'User5'],
+            title: {
+                text: null
+            }
+        },
+        legend: {
+            enabled: false,
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Submissions',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' submissions'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Submissions',
+            data: [50, 45, 24, 10, 2]
+        }]
+    });
+}
+
+function upvotedUsersBar() {
+    upvotedUserBarChart = Highcharts.chart('upvoted-users-bar', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Most Upvoted Users'
+        },
+        xAxis: {
+            categories: ['User1', 'User2', 'User3', 'User4', 'User5'],
+            title: {
+                text: null
+            }
+        },
+        legend: {
+            enabled: false,
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Upvotes',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' upvotes'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Upvotes',
             data: [50, 45, 24, 10, 2]
         }]
     });
