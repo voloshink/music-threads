02462d1be9bd430323280e0912d994412dfb2d3a
Kirill Voloshin <voloshin.k@husky.neu.edu>
2018-10-16 22:48:40 -0400

    no integers on the y axis for genre popularity line

--- website/js/genres.js
+++ website/js/genres.js
@@ -102,6 +102,7 @@ function popularityLine(subs, latestThread) {
             categories: data.map((_, i) => i),
         },
         yAxis: {
+            allowDecimals: false,
         },
         legend: {
             enabled: false,
