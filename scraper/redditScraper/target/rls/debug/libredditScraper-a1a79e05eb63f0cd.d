5c066481fa7c05b2714628356cab912107b75ad4
Kirill Voloshin <voloshin.k@husky.neu.edu>
2018-10-26 19:44:40 -0400

    starting reddit scraper

---
+++ scraper/redditScraper/target/rls/debug/libredditScraper-a1a79e05eb63f0cd.d
@@ -0,0 +1,1 @@
+c:\Users\polecat\projects\music-threads\scraper\redditScraper\target\rls\debug\libredditScraper-a1a79e05eb63f0cd.rmeta: c:\Users\polecat\projects\music-threads\scraper\redditScraper\src\main.rs
---
+++ scraper/redditScraper/target/rls/debug/libredditScraper.d
@@ -0,0 +1,1 @@
+c:\Users\polecat\projects\music-threads\scraper\redditScraper\target\rls\debug\libredditScraper.rmeta: c:\Users\polecat\projects\music-threads\scraper\redditScraper\src\main.rs
